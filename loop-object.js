e71dcd1520977b2b9ba80f6324015aa2ee3d6f19
zishan344 <zishanahmed344@gmail.com>
2022-02-26 13:30:43 +0600

    done loop object

--- loop-object.js
+++ loop-object.js
@@ -7,16 +7,16 @@ const student = {
 
 //start for in loop
 for (const prop in student) {
-  //   console.log(prop, student[prop]);
+  console.log(prop);
 }
 
-const keys = Object.keys(student);
-// console.log(keys);
-for (const prop of keys) {
-  //   console.log(prop, student[prop]);
-}
+// const keys = Object.keys(student);
+// // console.log(keys);
+// for (const prop of keys) {
+//   //   console.log(prop, student[prop]);
+// }
 
-const entris = Object.entries(student);
-for (const [key, value] of entris) {
-  console.log(key, value);
-}
+// const entris = Object.entries(student);
+// for (const [key, value] of entris) {
+//   console.log(key, value);
+// }
