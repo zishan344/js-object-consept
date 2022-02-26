d6464898e9432eb75df94b431a6bb4ef229f18b8
zishan344 <zishanahmed344@gmail.com>
2022-02-26 12:30:28 +0600

    done the 4 numbers video

---
+++ loop-object.js
@@ -0,0 +1,22 @@
+const student = {
+  name: "marouf",
+  age: 18,
+  home: "ctg",
+  isClean: true,
+};
+
+//start for in loop
+for (const prop in student) {
+  //   console.log(prop, student[prop]);
+}
+
+const keys = Object.keys(student);
+// console.log(keys);
+for (const prop of keys) {
+  //   console.log(prop, student[prop]);
+}
+
+const entris = Object.entries(student);
+for (const [key, value] of entris) {
+  console.log(key, value);
+}
