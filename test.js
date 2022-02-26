9ff82dcd02045667cc854a0751aea3aa60f5f838
zishan344 <zishanahmed344@gmail.com>
2022-02-26 18:00:27 +0600

    done the todays module

---
+++ test.js
@@ -0,0 +1,5 @@
+const obj1 = { module: 35, video: 2 };
+const obj2 = { module: 35, video: 2 };
+console.log(obj1 === obj2);
+const getGirlFriend = (name = "chokina") => "name";
+console.log(getGirlFriend());
